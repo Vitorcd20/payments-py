81e6e7145902d63a46e27b0fe7fedcc49dec63d6
Vitorcd20 <vitorvieeira12008@gmail.com>
2026-04-20 14:37:58 -0300

    feat: notifying the customer with confirmed payment

--- app.py
+++ app.py
@@ -46,28 +46,35 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
+
     data = request.get_json()
-    
+
     if "bank_payment_id" not in data and "value" not in data:
-        return jsonify({"message": 'Invalid payment data'}), 400
-    
-    # payment
+        return jsonify({"message": "Invalid payment data"}), 400
+
     payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
-    
+
     if not payment or payment.paid:
-       return jsonify({"message": 'Payment not found'}), 404
-        
+        return jsonify({"message": "Payment not found"}), 404
+
     if data.get("value") != payment.value:
-            return jsonify({"message": 'Invalid payment data'}), 400
-        
+        return jsonify({"message": "Invalid payment data"}), 400
+
     payment.paid = True
-    db.session.commit()   
-    
-    return jsonify({"message": 'the payment has been confirmed'})
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
+
+    return jsonify({"message": "The payment has been confirmed"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+
+    if payment.paid:
+        return render_template('confirmed_payment.html',
+                               payment_id=payment.id,
+                               value=payment.value
+                               )
 
     return render_template('payment.html',
                            payment_id=payment.id,
@@ -75,13 +82,11 @@ def payment_pix_page(payment_id):
                            host="http://127.0.0.1:5000",
                            qr_code=payment.qr_code,
                            )
-    
-        
+            
 # websockets
 @socketio.on('connect')
 def handle_connect():
     print("Client connected to the server")
-
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
